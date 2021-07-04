fd8b6990ed8cf5e80f4de252745044b62477675e
ozakione <29860391+OzakIOne@users.noreply.github.com>
2021-07-04 17:21:35 +0200

    fix(all): backup not working

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 import { writeFile } from 'fs';
 import fetch from 'node-fetch';
 import { promisify } from 'util';
+import pkg from 'node-html-parser';
+
+const { parse } = pkg;
 const writeFilePromise = promisify(writeFile);
 
 const YTPLAYLIST = process.argv[2];
 
-const URL = `http://www.williamsportwebdeveloper.com/FavBackUp.aspx?__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUJMjA1NTc2NjUyD2QWAmYPZBYCAgEPZBYCAhMPFQEgL2phdmFzY3JpcHQvanF1ZXJ5LTEuMTAuMi5taW4uanNkZPC5DyotSQBWeHVjBo2NGJw5nGL37CDysXH2I86s0CrV&__VIEWSTATEGENERATOR=0A525D99&ctl00%24ContentPlaceHolder1%24txtPlaylist=${YTPLAYLIST}&ctl00%24ContentPlaceHolder1%24btnSubmit=Submit`;
-
-const downloadFile = (url) => {
-  console.log(`Starting backup of ${YTPLAYLIST}`);
-  const now = new Date();
-  const fileName = `${now.getDate()}_${now.getMonth()}_${now.getFullYear()}.xls`;
-  fetch(url)
-    .then((x) => x.arrayBuffer())
-    .then((x) => {
-      writeFilePromise(fileName, Buffer.from(x));
-      console.log('Fetch ended');
-      // process.stdin.setRawMode(true);
-      // process.stdin.resume();
-      // process.stdin.on('data', process.exit.bind(process, 0));
-    });
+const createLink = async () => {
+  const rawData = await fetch(
+    'http://www.williamsportwebdeveloper.com/FavBackUp.aspx',
+  );
+  const HTML = await rawData.text();
+  const selector = await parse(HTML).querySelector('input[name="__VIEWSTATE"]');
+  const VIEWSTATE = encodeURIComponent(selector._rawAttrs.value);
+  return `http://www.williamsportwebdeveloper.com/FavBackUp.aspx?__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=${VIEWSTATE}&__VIEWSTATEGENERATOR=0A525D99&ctl00%24ContentPlaceHolder1%24txtPlaylist=${YTPLAYLIST}&ctl00%24ContentPlaceHolder1%24btnSubmit=Submit`;
 };
 
-downloadFile(URL);
+const downloadFile = () => {
+  console.log(`Starting backup of ${YTPLAYLIST}`);
+  const now = new Date();
+  const fileName = `${now.getDate()}_${now.getMonth() + 1}_${now.getFullYear()}.xls`;
+  createLink().then((url) => {
+    fetch(url)
+      .then((x) => x.arrayBuffer())
+      .then((x) => writeFilePromise(fileName, Buffer.from(x)));
+  });
+};
+
+downloadFile();
